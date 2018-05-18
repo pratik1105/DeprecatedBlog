2367f858fdd4e5fd6d9084e76179c36f0ffa327b
Pratik Mishra <pratikmishra001@gmail.com>
2018-05-18 18:18:22 +0530

    updated dependencies and resolved baseurl issue

--- js/main.js
+++ js/main.js
@@ -16,15 +16,15 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
-  
+
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  
-  if (window.location.pathname !== '/') {
+
+  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  
+
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
